f199fa5df14c50a3929316f8fcb01aa8faed6b0b
ali ka <ninja.armin@gmail.com>
2020-02-03 15:37:48 +0330

    Initial Commit

---
+++ Udder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Udder
+//
+//  Created by Ali Ka on 11/14/1398 AP.
+//  Copyright © 1398 AP Ali Ka. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
